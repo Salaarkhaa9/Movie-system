d1d907951afd689076c30862443b0b3dbe13e33e
Salaarkhaa9 <kowalskikh92@gmail.com>
2025-08-07 12:18:10 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import MovieCard from './components/MovieCard'
+function App() {
+  //CONDITIONAL RENDERING
+  const movienumber=1
+  return (
+    <>
+    {
+      movienumber===1?
+      (<MovieCard movie={{title:"Final Destination" , release_date:"August 2025"}}/>):
+      (<MovieCard movie={{title:"Spider man" , release_date:"June 2025"}}/>)
+    }
+    </>
+  )
+}
+
+export default App
