b5411306684f3e38d0243e0f5ce9200bbdc05682
Salaarkhaa9 <kowalskikh92@gmail.com>
2025-08-11 12:16:03 +0500

    Update context

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
-import './css/App.css'
-import Home from './pages/Home'
-import Favorites from './pages/Favorites'
-import {Routes,Route} from "react-router-dom"
-import NavBar from './components/NavBar'
+import "./css/App.css";
+import Home from "./pages/Home";
+import Favorites from "./pages/Favorites";
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import { MovieProvider } from "./context/MovieContext";
 function App() {
   return (
-    <div>
-      <NavBar/>
-      <main className='main-content'>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/favorites' element={<Favorites/>}/>
-      </Routes>
-    </main>
-    </div>
-  )
+    <MovieProvider>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
+    </MovieProvider>
+  );
 }
 
-export default App
+export default App;
