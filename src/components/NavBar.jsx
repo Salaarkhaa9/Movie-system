0390b46971be9eedc3219acb60b74e5d09d24647
Salaarkhaa9 <kowalskikh92@gmail.com>
2025-08-11 09:28:16 +0500

    Routing

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+function NavBar(){
+    return <div className="navbar">
+        <div className="navbar-brand">
+            <Link to="/">Movie App</Link>
+        </div>
+        <div className="navbar-link">
+            <Link to="/">Home</Link>
+            <Link to="/favorites">Favorites</Link>
+        </div>
+    </div>
+}
+
+export default NavBar
